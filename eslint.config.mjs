07c12657826b69d2d06c3f8e115ece846d13f648
Dat <nguyenquocdat2511998@gmail.com>
2024-08-18 23:44:46 +0700

    feat: add lint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["src/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    ignores: ["node_modules", "webpack.config.js"],
+  },
+  {
+    settings: {
+      react: {
+        version: "detect", // React version. "detect" automatically picks the version you have installed.
+        // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+        // Defaults to the "defaultVersion" setting and warns if missing, and to "detect" in the future
+      },
+    },
+  },
+];
